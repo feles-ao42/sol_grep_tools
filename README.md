d8e00dffc51d752b8ecedfd0655d205ec9c828c5
feles-ao42 <yf@keio.jp>
2023-07-04 11:46:43 +0900

    feat:output_to_txt

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # sol_grep_tools
 
+## Requiment
+- Language : python3
+- Library : bs4
+
 ## Installation
-- pip3 install bs4
+- `pip3 install bs4`
 
 ## Usage
 - `./html`に課題をコピーする。
 - python3 main.py
+- `output.txt`に出力結果が保存されます。
 
 ## Tips
 - main.py内のprintをコメントアウトすることで、取得箇所を変更可能です。
+- defaultはbodyだけになるように設定しています。
--- main.py
+++ main.py
@@ -4,29 +4,37 @@ from bs4 import BeautifulSoup
 # HTML ファイルが格納されているディレクトリのパス
 directory = "./html/"
 
+# 出力ファイルのパス
+output_file = "./output.txt"
+
 # ディレクトリ内のすべてのファイルに対して処理を行う
-for filename in os.listdir(directory):
-    if filename.endswith(".html"):
-        # HTML ファイルのパス
-        filepath = os.path.join(directory, filename)
+with open(output_file, "w", encoding="utf-8") as file:
+    for filename in os.listdir(directory):
+        if filename.endswith(".html"):
+            # HTML ファイルのパス
+            filepath = os.path.join(directory, filename)
 
-        # HTML ファイルを開く
-        with open(filepath, "r", encoding="utf-8") as file:
-            html = file.read()
+            # HTML ファイルを開く
+            with open(filepath, "r", encoding="utf-8") as html_file:
+                html = html_file.read()
 
-        # BeautifulSoup を使用して HTML を解析
-        soup = BeautifulSoup(html, "html.parser")
+            # BeautifulSoup を使用して HTML を解析
+            soup = BeautifulSoup(html, "html.parser")
 
-        # タイトルを取得
-        title = soup.title.string
+            # タイトルを取得
+            title = soup.title.string
 
-        # 本文を取得
-        body = ""
-        for paragraph in soup.find_all("p"):
-            body += paragraph.text + "\n"
+            # 本文を取得
+            body = ""
+            for paragraph in soup.find_all("p"):
+                text = paragraph.text.strip()  # 空白文字を除去
+                if text:  # 空でない場合のみ追加
+                    body += text + "\n"
 
-        # 結果を表示
-        # print("ファイル:", filename)
-        # print("タイトル:", title)
-        print("本文:\n", body)
-        print("---")
+            # 結果をテキストファイルに出力
+            if title:
+                #file.write("ファイル: {}\n".format(filename))
+                #file.write("タイトル: {}\n".format(title))
+                if body:
+                    file.write("本文:\n{}\n".format(body))
+                file.write("---\n")
